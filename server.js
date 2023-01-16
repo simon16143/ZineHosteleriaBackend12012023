c30e5316803a6a96456c92bea991dc003811f154
Symon <simon16143@gmail.com>
2023-01-16 10:38:39 -0500

    solving json error 16012023

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const app = express()
 const fetch = require("node-fetch")
+const bodyParser = require("body-parser");
 const cors = require("cors")
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
+app.use(bodyParser.json());
 const urlPost = ("https://accounts.zoho.eu/oauth/v2/token?refresh_token=1000.3dbdad6937dc0800c4dcc662cd14d173.86efb18e337989bebb3ff4c05582c94c&client_id=1000.NQL17JHK3Y62Y178TO0E3FQC6MBQJV&client_secret=5d04ad135862e7313377484af55efa1f41c1f49a39&grant_type=refresh_token")
 const urlGet = "https://creator.zoho.eu/api/v2/hostienda1/Product-Catalog/report/Product_Details";
 app.use(cors())
 
 const peticion = fetch(urlPost,{
       method: 'POST',
+      redirect: 'follow'
     });
     peticion
     .then((ans)=>{return ans.json()})
@@ -16,13 +19,12 @@ const peticion = fetch(urlPost,{
       const reslt = resp.access_token;
       return app.get("*", async (req,res)=>{
         const response = await fetch(urlGet,{
-        method: 'GET',
+        method: "GET",
         headers:{
             'Authorization':`Zoho-oauthtoken ${reslt}`,
-           }})
+          }})
           res.json(await response.json())
            
         })
       })
-
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)})
